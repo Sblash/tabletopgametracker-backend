7741bca81701a86483d6d7abe0edafe8a47356d5
Stefano <stecre@live.it>
2022-10-30 00:41:47 +0200

    fix cors

--- app/src/server.ts
+++ app/src/server.ts
@@ -43,6 +43,7 @@ if (process.env.NODE_ENV === 'production') {
 //CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
     next();      
   }); 
