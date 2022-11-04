0172ad821d40bb5b4b663ae3713141d1d3ecb2d7
Stefano <stecre@live.it>
2022-11-04 03:01:56 +0100

    added structure interface

---
+++ app/src/interfaces/Structure.ts
@@ -0,0 +1,18 @@
+export interface Structure {
+    rows: Array<Row>
+}
+
+export interface Row {
+    cols: Array<Col>
+}
+
+export interface Col {
+    elements: Array<Element>
+}
+
+export interface Element {
+    name: string,
+    type: string,
+    row_index: number | undefined,
+    col_index: number | undefined
+}
